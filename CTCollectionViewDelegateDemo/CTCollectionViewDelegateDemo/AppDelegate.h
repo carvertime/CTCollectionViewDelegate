d381f1b9640f80f032fe0ad593b32172348b7275
高文杰 <yidong-ios16@yhd.com>
2017-09-12 14:52:39 +0800

    init

---
+++ CTCollectionViewDelegateDemo/CTCollectionViewDelegateDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CTCollectionViewDelegateDemo
+//
+//  Created by wenjie on 2017/9/12.
+//  Copyright © 2017年 Demo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CTCollectionViewDelegateDemo/CTCollectionViewDelegateDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CTCollectionViewDelegateDemo
+//
+//  Created by wenjie on 2017/9/12.
+//  Copyright © 2017年 Demo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
